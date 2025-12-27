ed70154ac471b2828ae12602d3b2f1b40837c269
krPiyush2816 <krpiyush2828@gmail.com>
2025-12-28 00:02:53 +0530

    Refactor registration form input structure
    Refactor registration form inputs to use div wrappers for better styling.

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -30,23 +30,32 @@ const Register = () => {
     if (json.status) {
         sessionStorage.setItem('username', json.userName);
         window.location.href = window.location.origin;
-    } else if (json.error === "Already Registered") {
-        alert("The user with same username is already registered");
     }
   };
 
   return (
-    <div className="register_container">
-      <div className="header">
+    <div className="register_container" style={{width: "50%", margin: "auto"}}>
+      <div className="header" style={{display: "flex",flexDirection: "row", justifyContent: "space-between"}}>
         <span className="text">Sign-up</span>
       </div>
+      <hr/>
       <form onSubmit={register}>
         <div className="inputs">
-          <input type="text" name="first_name" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} required/>
-          <input type="text" name="last_name" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} required/>
-          <input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required/>
-          <input type="text" name="username" placeholder="Username" onChange={(e) => setUserName(e.target.value)} required/>
-          <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
+          <div className="input">
+            <input type="text" name="first_name" placeholder="First Name" className="input_field" onChange={(e) => setFirstName(e.target.value)} required/>
+          </div>
+          <div className="input">
+            <input type="text" name="last_name" placeholder="Last Name" className="input_field" onChange={(e) => setLastName(e.target.value)} required/>
+          </div>
+          <div className="input">
+            <input type="email" name="email" placeholder="Email" className="input_field" onChange={(e) => setEmail(e.target.value)} required/>
+          </div>
+          <div className="input">
+            <input type="text" name="username" placeholder="Username" className="input_field" onChange={(e) => setUserName(e.target.value)} required/>
+          </div>
+          <div className="input">
+            <input type="password" name="password" placeholder="Password" className="input_field" onChange={(e) => setPassword(e.target.value)} required/>
+          </div>
         </div>
         <div className="submit_container">
           <button type="submit" className="submit">Register</button>
